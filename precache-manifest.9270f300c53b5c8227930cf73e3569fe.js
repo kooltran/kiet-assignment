b017986a36fc7743dffb6c50d4800dc05b717ab1
kooltran <kiettrankm11@gmail.com>
2020-01-10 01:28:35 +0700

    Updates

---
+++ precache-manifest.9270f300c53b5c8227930cf73e3569fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e8521a9e46fea8dd51839f0f80dc791",
+    "url": "/kiet-assignment/index.html"
+  },
+  {
+    "revision": "09ef6df232f0fdd3e3df",
+    "url": "/kiet-assignment/static/js/2.d0da043a.chunk.js"
+  },
+  {
+    "revision": "7eb906b2d8ef627da08d7f4a543e6732",
+    "url": "/kiet-assignment/static/js/2.d0da043a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a4806e1a019672d221b5",
+    "url": "/kiet-assignment/static/js/main.68ec67dd.chunk.js"
+  },
+  {
+    "revision": "67b959fd71f98c2dce5e",
+    "url": "/kiet-assignment/static/js/runtime-main.83faab7d.js"
+  },
+  {
+    "revision": "e6e9715ce9532e66e882b3ed49db9324",
+    "url": "/kiet-assignment/static/media/loading.e6e9715c.svg"
+  }
+]);
